f20b68802870a14f3e8c4fec7e915abcc2cfedff
Thiago Sato <thiago.sato@shift.com.br>
2023-05-31 09:09:15 -0300

    Primeira Aula

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'a11y-p1';
+  public yesNoAnswer = 'no';
+}
